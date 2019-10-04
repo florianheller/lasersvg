ad59ffafc89ea6a2510629a51483886422012f01
Florian Heller <florian.heller@uhasselt.be>
2019-10-04 10:21:52 +0200

    New URL

--- library/lasersvg.js
+++ library/lasersvg.js
@@ -28,7 +28,7 @@
 
 const laser_NS = 'http://www.heller-web.net/lasersvg';
 const svg_NS = 'http://www.w3.org/2000/svg';
-const laserSvgURL = 'http://www2.heller-web.net/LaserSVG2/';
+const laserSvgURL = 'http://www2.heller-web.net/lasersvg/';
 // References to the different points in the DOM
 var parentDocument;
 var laserSvgDocument;
