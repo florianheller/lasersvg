ff4f44f3593fb2f7f38e6af858ec9d205127f613
Florian Heller <florian@heller-web.net>
2022-09-09 23:13:28 +0200

    adapted for automated deployment

--- library/deploy.sh
+++ library/deploy.sh
@@ -1,3 +1,2 @@
-#!/bin/zsh
-
-cat lasersvg.js lasersvgedit.js lasersvgjoints.js path-data-polyfill.js > lasersvgmain.js
+cat lasersvg.js lasersvgedit.js lasersvgjoints.js path-data-polyfill.js > ../docs/lasersvg.js
+cp lasersvg.css ../docs/
